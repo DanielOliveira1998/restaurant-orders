212aa38d357839d4985f1aab8528e2baba0d86b4
Daniel Oliveira <danoliveira865@gmail.com>
2023-09-13 20:00:32 -0300

    req 1, falta arrumar erro de flake8

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+# flake8 error:
+# ./tests/ingredient/test_ingredient.py:10:9: F821 undefined name 'Restriction'
+# ./tests/ingredient/test_ingredient.py:11:9: F821 undefined name 'Restriction'
 
 
 # Req 1
 def test_ingredient():
-    pass
+    fisrt_ingredient = Ingredient("farinha")
+    second_ingredient = Ingredient("farinha")
+    third_ingredient = Ingredient("bacon")
+    third_ingredient_restrictions = {
+        Restriction.ANIMAL_MEAT,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert hash(fisrt_ingredient) == hash(second_ingredient)
+    assert hash(third_ingredient) != hash(fisrt_ingredient)
+    assert fisrt_ingredient.__eq__(second_ingredient)
+    assert fisrt_ingredient.__eq__(fisrt_ingredient)
+    assert not third_ingredient.__eq__(fisrt_ingredient)
+    assert repr(fisrt_ingredient) == "Ingredient('ingredient A')"
+    assert fisrt_ingredient.name == "ingredient A"
+    assert third_ingredient.restrictions == third_ingredient_restrictions
